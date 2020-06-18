88822fcd6e9f9fed77301e359fdd7e5b51663841
amnjn01 <f20180768@goa.bits-pilani.ac.in>
2020-06-18 20:53:48 +0530

    Initial

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,107 @@
+import React, { Fragment, useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
+import axios from 'axios';
+
+const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [size_of_fragments, setSize_of_fragments] = useState('');
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+  const onChange2 = (e) => {
+    setSize_of_fragments(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log(size_of_fragments);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            )
+          );
+
+          // Clear percentage
+          setTimeout(() => setUploadPercentage(0), 10000);
+        },
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      {message ? <Message msg={message} /> : null}
+      <form onSubmit={onSubmit}>
+        <div className='custom-file mb-4'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label className='custom-file-label' htmlFor='customFile'>
+            {filename}
+          </label>
+          <div className='input-group mb-3 mt-3'>
+            <input
+              type='number'
+              value={size_of_fragments}
+              className='form-control'
+              placeholder='Enter Fragment Size in KB'
+              aria-label='Enter Fragment Size in KB'
+              aria-describedby='basic-addon1'
+              required
+              onChange={onChange2}
+            />
+          </div>
+        </div>
+
+        <Progress percentage={uploadPercentage} />
+
+        <input
+          type='submit'
+          value='Upload'
+          className='btn btn-primary btn-block mt-4'
+        />
+      </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default FileUpload;
